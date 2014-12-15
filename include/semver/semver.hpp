9a6ff177e851f4313178d02e1c6cb2ea84eed46b
Diego Lago <diego.lago.gonzalez@gmail.com>
2014-12-15 11:27:53 +0100

    Basic design of SemVer class.

---
+++ include/semver/semver.hpp
@@ -0,0 +1,39 @@
+#ifndef __SEMVER_HPP__
+#define __SEMVER_HPP__
+
+#include <string>
+
+namespace semver {
+
+using std::string;
+
+class SemVer {
+	private:
+
+		unsigned 	fMajor;
+		unsigned	fMinor;
+		unsigned	fPatch;
+		string		fLabel;
+
+		void		parseVersionString(const string version);
+
+	public:
+		SemVer();
+
+		SemVer(unsigned major, unsigned minor, unsigned patch, const string label = string());
+
+		SemVer(string version);
+
+		virtual ~SemVer();
+
+		virtual unsigned	major() const;
+		virtual unsigned	minor() const;
+		virtual unsigned	patch() const;
+		virtual string		label() const;
+
+		virtual int			compare(const SemVer& semver) const;
+};
+
+}	// namespace semver
+
+#endif
---
+++ src/semver.cpp
@@ -0,0 +1,57 @@
+#include "../include/semver/semver.hpp"
+
+namespace semver {
+
+SemVer::SemVer() {
+
+}
+
+SemVer::SemVer(unsigned major, unsigned minor, unsigned patch, const string label)
+	: fMajor(major),
+	  fMinor(minor),
+	  fPatch(patch),
+	  fLabel(label)
+{
+
+}
+
+SemVer::SemVer(const string version)
+	: fMajor(0),
+	  fMinor(0),
+	  fPatch(0),
+	  fLabel()
+{
+	parseVersionString(version);
+}
+
+SemVer::~SemVer() {
+
+}
+
+void
+SemVer::parseVersionString(const string version) {
+	// TODO: Implement this.
+}
+
+unsigned
+SemVer::major() const {
+	return fMajor;
+}
+
+unsigned
+SemVer::minor() const {
+	return fMinor;
+}
+
+unsigned
+SemVer::patch() const {
+	return fPatch;
+}
+
+int
+SemVer::compare(const SemVer& semver) const {
+	// TODO: Implement this.
+	return 0;
+}
+
+}	// namespace semver
