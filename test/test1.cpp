9057f272416379f683cf313ece22906856967566
Diego Lago <diego.lago.gonzalez@gmail.com>
2014-12-16 16:52:26 +0100

    Better tests.

--- test/test1.cpp
+++ test/test1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <exception>
 
 using namespace std;
 
@@ -8,9 +9,28 @@ int
 main() {
 	cout << "SemVer Class Test" << endl;
 
-	semver::SemVer* s = new semver::SemVer();
+	semver::SemVer* s = new semver::SemVer("1.2.3");
 
-	cout << s->toString() << endl;
+	int SIZE = 8;
+	string ss[] = {
+		"",
+		"1",
+		"1.2",
+		"1.2.3",
+		"1.2.3-alpha",
+		"1.2.3-alpha+build",
+		"0.0.0+build_metadata",
+		"0.0.0+build-metadata"	// does not work
+	};
+
+	for (int i = 0; i < SIZE; i++) {
+		try {
+			s->assign(ss[i]);
+			cout << (ss[i] == s->toString() ? "  OK" : "FAIL") << ": " << ss[i] << " =? " << s->toString() << endl;
+		} catch(std::exception& e) {
+			cerr << "ERROR: " << e.what() << endl;
+		}
+	}
 
 	return 0;
 }
