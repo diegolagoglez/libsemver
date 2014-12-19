0339663663d50aca38e46e0f4d6a2b08de1b1a82
Diego Lago <diego.lago.gonzalez@gmail.com>
2014-12-19 11:12:19 +0100

    Comparison tests.

--- test/comparisons.cpp
+++ test/comparisons.cpp
@@ -5,6 +5,16 @@ using namespace std;
 
 #include <semver/semver.hpp>
 
+void
+showComparisons(const semver::SemVer& a, const semver::SemVer& b) {
+	cout << "'" << a << "' == '" << b << "' = " << (a == b ? "yes" : "no") << endl;
+	cout << "'" << a << "' != '" << b << "' = " << (a != b ? "yes" : "no") << endl;
+	cout << "'" << a << "' <  '" << b << "' = " << (a < b ? "yes" : "no") << endl;
+	cout << "'" << a << "' >  '" << b << "' = " << (a > b ? "yes" : "no") << endl;
+	cout << "'" << a << "' <= '" << b << "' = " << (a <= b ? "yes" : "no") << endl;
+	cout << "'" << a << "' >= '" << b << "' = " << (a >= b ? "yes" : "no") << endl;
+}
+
 int
 main() {
 	cout << "SemVer Class Test" << endl;
@@ -21,6 +31,10 @@ main() {
 	a->assign("1.2.3");
 	b->assign("1.2.3");
 	cout << "'" << *a << "' cmp '" << *b << "' = " << a->compare(b) << endl;
+
+	showComparisons(*a, *b);
+
+	showComparisons(semver::SemVer("0.0.1"), semver::SemVer("0.0.2"));
 
 	return 0;
 }
