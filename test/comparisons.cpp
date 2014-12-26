4c5db923a494f4f8872f8d3b798eef0e34884df0
Diego Lago <diego.lago.gonzalez@gmail.com>
2014-12-26 13:55:58 +0100

    Better comparisons test.

--- test/comparisons.cpp
+++ test/comparisons.cpp
@@ -5,36 +5,62 @@ using namespace std;
 
 #include <semver/semver.hpp>
 
+typedef struct {
+	string a;
+	string b;
+	int comparison;
+} TestVersion;
+
+static TestVersion versions[] = {
+	{ "", "", 0 },
+	{ "0", "0", 0 },
+	{ "0.0", "0.0", 0 },
+	{ "0.0.0", "0.0.0", 0 },
+	{ "0.0.1", "0.0.1", 0 },
+	{ "0.0.1", "0.0.2", -1 },
+	{ "0.0.2", "0.0.1", 1 },
+	{ "1.2.3", "1.2.4", -1 },
+	{ "1.2.3", "1.3.3", -1 },
+	{ "1.2.3", "1.3.2", -1 },
+	{ "2.5.6", "1.9.9", 1 },
+	{ "5.0.0", "4.0.0", 1 },
+	{ "0.98.0", "0.97.0", 1 },
+	{ "6.0.254", "6.0.255", -1 },
+	{ "0.0.1-alpha", "0.0.1-alpha1", -1 },
+	{ "0.0.1-alpha", "0.0.1-beta", -1 },
+	{ "0.0.1-alpha-3", "0.0.1-alpha-2", 1 },
+};
+
 void
-showComparisons(const semver::SemVer& a, const semver::SemVer& b) {
+showComparisons(const semver::SemVer& a, const semver::SemVer& b, int shouldBe) {
+	cout << "'" << a << "' cmp '" << b << "' = " << a.compare(b) << " (should be: " << shouldBe << ")" << endl;
 	cout << "'" << a << "' == '" << b << "' = " << (a == b ? "yes" : "no") << endl;
 	cout << "'" << a << "' != '" << b << "' = " << (a != b ? "yes" : "no") << endl;
 	cout << "'" << a << "' <  '" << b << "' = " << (a < b ? "yes" : "no") << endl;
 	cout << "'" << a << "' >  '" << b << "' = " << (a > b ? "yes" : "no") << endl;
 	cout << "'" << a << "' <= '" << b << "' = " << (a <= b ? "yes" : "no") << endl;
 	cout << "'" << a << "' >= '" << b << "' = " << (a >= b ? "yes" : "no") << endl;
+	cout << "---------------------------------------" << endl;
 }
 
 int
 main() {
 	cout << "SemVer Class Test" << endl;
 
-	semver::SemVer* a = new semver::SemVer("0.0.1");
-	semver::SemVer* b = new semver::SemVer("0.0.2");
+	semver::SemVer* a = new semver::SemVer();
+	semver::SemVer* b = new semver::SemVer();
 
-	cout << "'" << *a << "' cmp '" << *b << "' = " << a->compare(b) << endl;
+	for (int i = 0; i < sizeof(versions) / sizeof(TestVersion); i++) {
+		cout << "Testing: '" << versions[i].a << "' -- '" << versions[i].b << "'" << endl;
+		try {
+			a->assign(versions[i].a);
+			b->assign(versions[i].b);
 
-	a->assign("0.0.2");
-	b->assign("0.0.1");
-	cout << "'" << *a << "' cmp '" << *b << "' = " << a->compare(b) << endl;
-
-	a->assign("1.2.3");
-	b->assign("1.2.3");
-	cout << "'" << *a << "' cmp '" << *b << "' = " << a->compare(b) << endl;
-
-	showComparisons(*a, *b);
-
-	showComparisons(semver::SemVer("0.0.1"), semver::SemVer("0.0.2"));
+			showComparisons(*a, *b, versions[i].comparison);
+		} catch(exception& e) {
+			cout << "   ERROR: " << e.what() << endl;
+		}
+	}
 
 	return 0;
 }
